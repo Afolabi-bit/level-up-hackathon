f03a53cb53fbe82187a0e0c408371303583819d3
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-11-22 00:45:53 +0100

    wh

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const alertBtn = document.getElementById("alert-btn");
 const alertPopup = document.getElementById("alerts");
 
 userBtn.addEventListener("click", () => {
+  alertPopup.classList.remove("show");
   userPopup.classList.toggle("show");
 });
 
 alertBtn.addEventListener("click", () => {
+  userPopup.classList.remove("show");
   alertPopup.classList.toggle("show");
 });
