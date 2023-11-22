b722e12221771981f680422b84f006b7de6a229b
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-11-22 08:38:06 +0100

    setup

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const userPopup = document.getElementById("user-popup");
 const alertBtn = document.getElementById("alert-btn");
 const alertPopup = document.getElementById("alerts");
 const closeModalBtn = document.getElementById("close-modal");
+const count = document.getElementById("count");
+const progressBar = document.getElementById("progress-bar");
+const ink = document.getElementById("ink");
 
 userBtn.addEventListener("click", () => {
   alertPopup.classList.remove("show");
@@ -17,3 +20,5 @@ alertBtn.addEventListener("click", () => {
 closeModalBtn.addEventListener("click", (e) => {
   e.target.parentElement.remove();
 });
+
+ink.style.width = `${(+count.textContent / 5) * 120}px`;
