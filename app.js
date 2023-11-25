94e5557c39d6be49a51c4458b155c585f65e0087
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-11-25 23:27:05 +0100

    user btn accessibility

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const indicators = document.querySelectorAll(".indicator");
 const toggleBtns = document.querySelectorAll(".toggle-card");
 const cards = document.querySelectorAll(".card");
 
+// Calulates the length of the ink in the progress bar
 function calcProgressBar() {
   const checked = document.querySelectorAll(".checked");
   if (checked.length < 1) {
@@ -23,12 +24,26 @@ function calcProgressBar() {
   console.log(checked.length);
 }
 
+// Default call that sets the progress bar and ink length
 calcProgressBar();
 
+// Toggles the alerts button
+alertBtn.addEventListener("click", () => {
+  userPopup.classList.remove("show");
+  alertPopup.classList.toggle("show");
+});
+
+// Closes the trial modal
+closeModalBtn.addEventListener("click", (e) => {
+  e.target.parentElement.remove();
+});
+
+// Opens the setup guide
 arrow.addEventListener("click", () => {
   document.getElementById("guide").classList.toggle("open");
 });
 
+// Adds the status of each step of the guide to the progress bar
 indicators.forEach((ind) => {
   ind.addEventListener("click", () => {
     ind.classList.toggle("checked");
@@ -36,20 +51,7 @@ indicators.forEach((ind) => {
   });
 });
 
-userBtn.addEventListener("click", () => {
-  alertPopup.classList.remove("show");
-  userPopup.classList.toggle("show");
-});
-
-alertBtn.addEventListener("click", () => {
-  userPopup.classList.remove("show");
-  alertPopup.classList.toggle("show");
-});
-
-closeModalBtn.addEventListener("click", (e) => {
-  e.target.parentElement.remove();
-});
-
+// Toggles each item on the setup guide
 toggleBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     cards.forEach((card) => {
@@ -58,3 +60,70 @@ toggleBtns.forEach((btn) => {
     btn.parentElement.classList.add("expand");
   });
 });
+
+// ********************
+
+// ********************
+
+// Accessibilty programming
+
+// ********************
+
+// ********************
+let focusAbleMenuItems = userPopup.querySelectorAll('[role="menuitem"]');
+
+function closeMenu() {
+  userBtn.ariaExpanded = "false";
+  userBtn.focus();
+  userPopup.classList.remove("show");
+}
+
+function closeWithEscape(e) {
+  if (e.key == "Escape") {
+    closeMenu();
+  }
+}
+
+function openMenu() {
+  userBtn.ariaExpanded = "true";
+  focusAbleMenuItems.item(0).focus();
+
+  userPopup.addEventListener("keyup", (e) => closeWithEscape(e));
+}
+
+const toggleMenu = () => {
+  let isExpanded = userBtn.attributes["aria-expanded"].value;
+
+  alertPopup.classList.remove("show");
+  userPopup.classList.toggle("show");
+
+  if (isExpanded === "true") {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+};
+userBtn.addEventListener("click", toggleMenu);
+
+focusAbleMenuItems.forEach((menuitem, menuIndex) => {
+  menuitem.addEventListener("keyup", (e) => arrowKeyNavigation(e, menuIndex));
+});
+
+function arrowKeyNavigation(e, menuIndex) {
+  let nextItem = focusAbleMenuItems.item(menuIndex + 1);
+  let prevItem = focusAbleMenuItems.item(menuIndex - 1);
+
+  if (e.key == "ArrowDown" || e.key == "ArrowRight") {
+    if (menuIndex == focusAbleMenuItems.length - 1) {
+      focusAbleMenuItems.item(0).focus();
+      return;
+    }
+    nextItem.focus();
+  }
+  if (e.key == "ArrowUp" || e.key == "ArrowLeft") {
+    if (menuIndex == 0) {
+      focusAbleMenuItems.item(focusAbleMenuItems.length - 1).focus();
+    }
+    prevItem.focus();
+  }
+}
